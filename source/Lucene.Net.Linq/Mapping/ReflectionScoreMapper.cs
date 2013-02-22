627ff7c1c513269739ff74c082fbe6ed1fb96995
Chris van de Steeg <chris@chrisvandesteeg.nl>
2013-02-22 23:12:02 +0100

    Weird conflict error

--- source/Lucene.Net.Linq/Mapping/ReflectionScoreMapper.cs
+++ source/Lucene.Net.Linq/Mapping/ReflectionScoreMapper.cs
@@ -1,76 +1,4 @@
-<<<<<<< HEAD
-﻿using System;
-using System.Reflection;
-using Lucene.Net.Analysis;
-using Lucene.Net.Documents;
-using Lucene.Net.Linq.Search;
-using Lucene.Net.Search;
-
-namespace Lucene.Net.Linq.Mapping
-{
-    internal class ReflectionScoreMapper<T> : IFieldMapper<T>
-    {
-        private readonly PropertyInfo propertyInfo;
-
-        public ReflectionScoreMapper(PropertyInfo propertyInfo)
-        {
-            this.propertyInfo = propertyInfo;
-        }
-
-        public void CopyToDocument(T source, Document target)
-        {
-        }
-
-        public void CopyFromDocument(Document source, IQueryExecutionContext context, T target)
-        {
-            if (context.Phase == QueryExecutionPhase.ConvertResults)
-            {
-                var score = context.CurrentScoreDoc.Score;
-                propertyInfo.SetValue(target, score, null);
-            }
-        }
-
-        public SortField CreateSortField(bool reverse)
-        {
-            if (reverse)
-            {
-                return new SortField(SortField.FIELD_SCORE.Field, SortField.FIELD_SCORE.Type, true);
-            }
-            
-            return SortField.FIELD_SCORE;
-        }
-
-        public string ConvertToQueryExpression(object value)
-        {
-            throw new NotSupportedException();
-        }
-
-        public string EscapeSpecialCharacters(string value)
-        {
-            throw new NotSupportedException();
-        }
-
-        public Query CreateQuery(string pattern)
-        {
-            throw new NotSupportedException();
-        }
-
-        public Query CreateRangeQuery(object lowerBound, object upperBound, RangeType lowerRange, RangeType upperRange)
-        {
-            throw new NotSupportedException();
-        }
-
-        public object GetPropertyValue(T source)
-        {
-            return 0;
-        }
-
-        public string PropertyName { get { return propertyInfo.Name; } }
-        public string FieldName { get { return null; } }
-        public Analyzer Analyzer { get { return null; } }
-    }
-=======
-﻿using System;
+using System;
 using System.Reflection;
 using Lucene.Net.Analysis;
 using Lucene.Net.Documents;
@@ -116,6 +44,11 @@ namespace Lucene.Net.Linq.Mapping
             throw new NotSupportedException();
         }
 
+        public string EscapeSpecialCharacters(string value)
+        {
+            throw new NotSupportedException();
+        }
+
         public Query CreateQuery(string pattern)
         {
             throw new NotSupportedException();
@@ -135,5 +68,4 @@ namespace Lucene.Net.Linq.Mapping
         public string FieldName { get { return null; } }
         public Analyzer Analyzer { get { return null; } }
     }
->>>>>>> Lucene 2.9 compatible version
 }
